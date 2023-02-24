0fb7a59bfda3ac2ce23f1cb2550014d809fa4333
Bosse-Jansson-Nti-Johanneberg <119052305+Bosse-Jansson-Nti-Johanneberg@users.noreply.github.com>
2023-02-24 14:27:25 +0100

    Clamps för fonts och lade till ett erbjudande
    Jfgrohnerui gnerij

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let hamburger_button = document.querySelector(".hamburger_button");
+let hamburger_menu = document.querySelector(".hamburger_menu");
+
+
+hamburger_button.addEventListener("click, openMenu");
+
+function openMenu();
+hamburger_menu.display = flex; 
+hamburger_menu.style.animation="openMenu 2s linear";
