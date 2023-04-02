4878ae3fd509635c3e1ef96204176851a46bf45f
Bosse-Jansson-Nti-Johanneberg <119052305+Bosse-Jansson-Nti-Johanneberg@users.noreply.github.com>
2023-04-02 20:44:40 +0200

    Validerade kod
    Validerade koden

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("funkar");
+var time = getTime()
 
-    // --- The navigation --- //
-    let button = document.querySelector("i");
-    console.log(button)
-    let menu = document.querySelector("nav");
-    let certificate = document.querySelector("certificate");
-
-    button.addEventListener("click", show);
-
-    function show() {
-        console.log("funkar");
-        menu.classList.toggle("show");
-        certificate.visibility = "hidden";
-    }
-});
-
-
+document.getElementById
